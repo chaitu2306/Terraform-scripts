ee1e3f760f5ec3ec19715d5726f016b441621439
Chaithanya <ccc.chaitu2306@gmail.com>
2025-11-28 17:03:36 +0530

    terraform day2 commited

--- build.tf
+++ build.tf
@@ -77,3 +77,63 @@ resource "aws_route_table_association" "ass_private" {
     subnet_id = aws_subnet.private_subnet.id
     route_table_id = aws_route_table.rt2.id
 }
+
+
+resource "aws_security_group" "sg" {
+    name = "chaitu-sg-id"
+    description = "Allow TLS inbound traffic and all outbound traffic"
+    vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "chaitu-sg"
+    }
+    ingress {
+        from_port = 22
+        to_port =22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQNU9PugbWfkyNNLv6QHoe3417bGuIo7maOSqzoT97ZZSn2+u8TRfwAZt6zBXgPjHSK/a91ZxjIC3ms0v6IUVL+rB+GG90a5lO4AWDo1Rr/1XrwbaN1RqfogIHi/udGONZ1H5AUIl+AT3TA0T7qHaM+dPJilPgk1Ug5sMO5ZEMdHog91U2rM4ei7INIPGwvoMhSAwYSjrVkldJLrQcWX4Kylghi1E63RBbZgdxEYrtCTxacuIksBshoX2l1+2SIHGJqvlZNwL0XRmxSXqZdTJYu6jHH3WyZOm5NoNdAG5cGrSe1OgrOsz7DrKoO6ryM4cjETzbjTVC+8jqtT3Bm0POSDxJoTstHmhEF0KuJ9hIMIJ2SN22twwfdNK+mltp/S6RMw8r5A/pfmQpP1n0Z9ciS9H/NsgxoPC0aK1di99o0IAVpt09dUn9oQoYqq9n1CBGUuCRLxmRyVZ86p9JJF3q6RFMUqwIbRWZl5YAwA6CCUy7NHXkSPFh2j4ANPruijXlgkhDqMEa1op/iB2ztmqAq5/FVfeEaCrZkl0zr6gIPXZp0usoLcil23x3s+wJPiAogTZKcvFIq0BYrSSbHlYzudKgQ1ngX8ynt2vrhVPBWMunEUIXF68wbct82TIcZpD23golS6LE3MTJ7wcFBL44K3qypDT4tP76CMm9018/7EFQ== chait@Chaitu"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0ecb62995f68bb549"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.sg.id ]
+  subnet_id = aws_subnet.public_subnet.id
+  key_name = "deployer-key"
+  associate_public_ip_address =true
+  tags = {
+    Name = "chaitu-pub-ins"
+  }
+}
+
+resource "aws_instance" "private_web" {
+    ami = "ami-0ecb62995f68bb549"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ aws_security_group.sg.id ]
+    subnet_id = aws_subnet.private_subnet.id
+    key_name = "deployer-key"
+    associate_public_ip_address = false
+    tags = {
+        Name = "chaitu-pvt-ins"
+    }
+}
